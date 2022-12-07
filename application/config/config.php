020dac7d4e6aa0dade3c3176e182b6b08a15676d
azariahconcepcion19 <azariahconcepcion@gmail.com>
2022-12-07 13:48:17 +0800

    Change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://critiquehallapi.herokuapp.com/';
+$config['base_url'] = 'https://azariahconcepcion19.github.io/CritiqueHall-Api/';
 
 /*
 |--------------------------------------------------------------------------
